fed5314d1dcfbe81ed186b4d5fc2f52cd2539660
Mikolaj Krysiak <mikolajkrysiak.03@gmail.com>
2026-01-10 13:27:44 +0100

    functional client

--- client.c
+++ client.c
@@ -4,13 +4,32 @@
 #include <stdlib.h>
 #include <errno.h>
 #include <string.h>
+#include <signal.h>
 #include <sys/types.h>
 #include <sys/socket.h>
 #include <netinet/in.h>
 #include <netdb.h>
 #include <unistd.h>
+#include <pthread.h>
+
+#include "TLV.h"
 
 #define SA struct sockaddr
+
+int sockfd = 0;
+
+size_t user_s = 0;
+size_t pass_s = 0;
+char* username = NULL;
+char* password = NULL;
+
+void* send_thread(void* arg);
+void* recv_thread(void* arg);
+
+void sig_sigpipe(int signo) {
+	printf("Received SIGPIPE - exit\n");
+	exit(EXIT_FAILURE);
+}
 
 int main(int argc, char **argv) {
 
@@ -39,8 +58,27 @@ int main(int argc, char **argv) {
 		exit(EXIT_FAILURE);
 	}
 
+	printf("username:\n");
+	getline(&username, &user_s, stdin);
+	printf("password:\n");
+	getline(&password, &pass_s, stdin);
+	for(size_t i = 0; i < user_s; i++) {
+		if(username[i] == '\n') username[i] = '\0';
+	}
+	for(size_t i = 0; i < pass_s; i++) {
+		if(password[i] == '\n') password[i] = '\0';
+	}
 
-	int sockfd = 0;
+	struct sigaction action;
+	action.sa_handler = sig_sigpipe;
+	sigemptyset (&action.sa_mask);
+	action.sa_flags = 0;
+	int ret = sigaction(SIGPIPE, &action, NULL);
+	if(ret < 0) {
+		int err = errno;
+		printf("sigaction() ERROR %d : %s\n", err, strerror(err));
+		exit(EXIT_FAILURE);
+	}
 
 	sockfd = socket(AF_INET, SOCK_STREAM, 0);
 
@@ -50,14 +88,101 @@ int main(int argc, char **argv) {
 		exit(err);
 	}
 
-	int res = connect(sockfd, (SA *)&servaddr, sizeof(servaddr));
+	ret = connect(sockfd, (SA *)&servaddr, sizeof(servaddr));
 
-	if(res != 0) {
+	if(ret != 0) {
 		int err = errno;
 		printf("connect() ERROR %d: %s", err, strerror(err));
 		exit(err);
 	}
-	sleep(10); // whole code here
+
+	pthread_t sendt;
+	pthread_t recvt;
+
+	pthread_create(&sendt, NULL, send_thread, NULL);
+	pthread_create(&recvt, NULL, recv_thread, NULL);
+
+	pthread_join(sendt, NULL);
+	pthread_join(recvt, NULL);
 
 	exit(EXIT_SUCCESS);
+}
+
+void* send_thread(void* arg) {
+	while(1) {
+		size_t n = 0;
+		char* text = NULL;
+		getline(&text, &n, stdin);
+		if(!strcmp(text, "QUIT\n")) {
+			printf("QUITTING...\n");
+			struct tlv msg;
+			msg.length = sprintf(text, "TERM\n");
+			msg.type = CONTROL;
+			msg.data = (uint8_t*)text;
+			send_tlv(sockfd, &msg);
+			close(sockfd);
+			exit(EXIT_SUCCESS);
+			pthread_exit(NULL);
+		}
+		struct tlv msg;
+		msg.type = MESSAGE;
+		msg.length = strlen(text);
+		msg.data = (uint8_t*)text;
+		send_tlv(sockfd, &msg);
+	}
+
+	return NULL;
+}
+
+void* recv_thread(void* arg) {
+	struct tlv receives;
+	receives.data = NULL;
+	struct tlv msg;
+	msg.data = NULL;
+	while (1) {
+		recv_tlv(sockfd, &receives);
+		char *text = NULL;
+		switch (receives.type) {
+			case MESSAGE:
+				text = malloc(receives.length + 1);
+				memset(text, 0, receives.length + 1);
+				memcpy(text, receives.data, receives.length);
+				printf("%s", text);
+				free(text);
+				text = NULL;
+				break;
+			case CONTROL:
+				if(strcmp((char*)receives.data, "TERM\n")) {
+					printf("QUITTING...\n");
+					close(sockfd);
+					exit(EXIT_SUCCESS);
+				}else if(strcmp((char*)receives.data, "REJECT\n")) {
+					printf("Server busy :(\n");
+					close(sockfd);
+					exit(EXIT_SUCCESS);
+				}else if(strcmp((char*)receives.data, "SERV_ERROR\n")) {
+					printf("Server error :(\n");
+					close(sockfd);
+					exit(EXIT_SUCCESS);
+				}
+				break;
+			case NAME_REQ:
+				msg.type = NAME;
+				msg.data = (uint8_t*)username;
+				msg.length = strlen(username);
+				send_tlv(sockfd, &msg);
+				break;
+			case PASS_REQ:
+				msg.type = PASS;
+				msg.data = (uint8_t*)password;
+				msg.length = strlen(password);
+				send_tlv(sockfd, &msg);
+				break;
+			default:
+			printf("WRONG DATA RECEIVED %X %X\n", MESSAGE, receives.type);
+			exit(EXIT_FAILURE);
+		}
+		free(text);
+	}
+	return NULL;
 }

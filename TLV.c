24f1e05ccf3941731df11f67a1115463eae617a4
Mikolaj Krysiak <mikolajkrysiak.03@gmail.com>
2026-01-24 23:55:22 +0100

    attempt to make better read

--- TLV.c
+++ TLV.c
@@ -1,7 +1,5 @@
 
 #include "TLV.h"
-#include <stdlib.h>
-#include <string.h>
 
 int send_tlv(int sockfd, struct tlv* tlvs) {
 	size_t buff_size = sizeof(tlvs->length) + sizeof(tlvs->type) + tlvs->length;
@@ -9,15 +7,17 @@ int send_tlv(int sockfd, struct tlv* tlvs) {
 	memcpy((buff), &(tlvs->type), sizeof(tlvs->type));
 	memcpy((buff + sizeof(tlvs->type)), &(tlvs->length), sizeof(tlvs->length));
 	memcpy((buff + sizeof(tlvs->type) + sizeof(tlvs->length)), (tlvs->data), tlvs->length);
-	int ret = send(sockfd, buff, buff_size, 0);
+	int ret = sendAll(sockfd, buff, buff_size);
 	if(ret < 0) return ret;
 	free(buff);
 	return ret;
 }
 
+#ifdef OLDRECV
+
 int recv_tlv(int sockfd, struct tlv* tlvs) {
 	uint8_t* buff = malloc(MAX_RECV_BUFF);
-	size_t received = recv(sockfd, buff, MAX_RECV_BUFF, 0);
+	int received = recv(sockfd, buff, MAX_RECV_BUFF, 0);
 	if(received < (sizeof(tlvs->type) + sizeof(tlvs->length))) {
 		free(buff);
 		tlvs->type = INVALID;
@@ -29,6 +29,58 @@ int recv_tlv(int sockfd, struct tlv* tlvs) {
 	memcpy((tlvs->data), (buff + sizeof(tlvs->type) + sizeof(tlvs->length)), tlvs->length);
 	free(buff);
 	return 0;
+}
+
+#else
+
+int recv_tlv(int sockfd, struct tlv* tlvs) {
+	uint8_t *buff = malloc(sizeof(tlvs->length) + sizeof(tlvs->type));
+	int ret = readLength(sockfd, buff, sizeof(tlvs->length) + sizeof(tlvs->type));
+	if(ret < 0) {
+		free(buff);
+		tlvs->type = INVALID;
+		return -1;
+	}
+	memcpy(&(tlvs->type), (buff), sizeof(tlvs->type));
+	memcpy(&(tlvs->length), (buff + sizeof(tlvs->type)), sizeof(tlvs->length));
+	if(tlvs->length == 0) return 0;
+	tlvs->data = malloc(tlvs->length);
+	free(buff);
+	ret = readLength(sockfd, tlvs->data, tlvs->length);
+	if(ret < 0) {
+		free(tlvs->data);
+		tlvs->type = INVALID;
+		return -1;
+	}
+	return 0;
+}
+
+#endif
+
+int sendAll(int sockfd, uint8_t *buff, size_t len) {
+	size_t sended = 0;
+	size_t tosend = len;
+	int ret;
+	while(sended < len) {
+		ret = write(sockfd, buff + sended, tosend);
+		if(ret == -1) return -1;
+		sended += ret;
+		tosend -= ret;
+	}
+	return 0;
+}
+
+int readLength(int sockfd, uint8_t *buff, size_t length) {
+	size_t received = 0;
+	size_t torecv = length;
+	int ret = 0;
+	while(torecv > 0) {
+		ret = read(sockfd, buff + received, torecv);
+		if(ret == -1) return -1;
+		received += ret;
+		torecv -= ret;
+	}
+	return received;
 }
 
 int sendMessage(int sockfd, int type, char* message) {
--- TLV.h
+++ TLV.h
@@ -7,9 +7,13 @@
 #include <stdlib.h>
 #include <string.h>
 #include <errno.h>
+#include <unistd.h>
 #include <sys/socket.h>
+#include <sys/ioctl.h>
 
 #define MAX_RECV_BUFF 20000
+
+#define OLDRECV
 
 enum types {
 	INVALID,
@@ -29,6 +33,9 @@ struct tlv {
 
 int send_tlv(int sockfd, struct tlv* tlvs);
 int recv_tlv(int sockfd, struct tlv* tlvs);
+
+int sendAll(int sockfd, uint8_t *buff, size_t len);
+int readLength(int sockfd, uint8_t *buff, size_t length);
 
 int sendMessage(int sockfd, int type, char* message);
 
